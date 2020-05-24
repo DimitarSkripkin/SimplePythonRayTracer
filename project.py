027d86a1b716ad62369462b9165016e3dc7c9baa
Dimitar Skripkin <dimitar.skripkin@gmail.com>
2020-05-25 02:56:28 +0300

    Refactored user interface.

--- project.py
+++ project.py
@@ -1,9 +1,60 @@
 
 import logging
+import time
+from datetime import datetime
+
 import wx
 
-from scene_data import Scene
 from ray_tracer_renderer import RayTracerRenderer
+from scene_data import Scene
+
+class ControlsPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, style=wx.SUNKEN_BORDER)
+
+        self.renderBtn = wx.Button(self, label='Render')
+        self.saveBtn = wx.Button(self, label='Save')
+        self.stats = wx.StaticText(self, label='Stats: ')
+
+        h_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        h_sizer.Add(self.stats, 1, wx.ALL | wx.EXPAND, 5)
+        h_sizer.Add(self.renderBtn, 0, wx.ALL | wx.ALIGN_CENTER, 5)
+        h_sizer.Add(self.saveBtn, 0, wx.ALL | wx.ALIGN_CENTER, 5)
+
+        self.SetSizerAndFit(h_sizer)
+
+    def UpdateStats(self, stats):
+        label = f'Stats: {", ".join(stats)}'
+        self.stats.SetLabel(label)
+
+class RenderPanel(wx.Panel):
+    def __init__(self, parent, frame):
+        wx.Panel.__init__(self, parent)
+
+        self.bitmap = self.MakeBitmap(frame, frame.width, frame.height)
+        self.img = wx.StaticBitmap(self, 0, self.bitmap)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.img, 1, wx.ALL | wx.SHAPED, 0)
+
+        self.SetSizerAndFit(sizer)
+
+    def UpdateRender(self, frame):
+        virtual_size = self.GetVirtualSize()
+        self.bitmap = self.MakeBitmap(frame, virtual_size.width, virtual_size.height)
+        self.img.SetBitmap(self.bitmap)
+
+    def MakeBitmap(self, frame, target_width, target_height):
+        if target_width <= 0:
+            target_width = 1
+        if target_height <= 0:
+            target_height = 1
+
+        # Make a bitmap using an array of RGBA bytes
+        bitmap = wx.Bitmap.FromBufferRGBA(frame.width, frame.height, frame.bytes)
+        image = bitmap.ConvertToImage().Mirror(False)
+        # image = image.Scale(target_width, target_height, wx.IMAGE_QUALITY_HIGH)
+        image = image.Scale(target_width, target_height, wx.IMAGE_QUALITY_NEAREST)
+        return wx.Bitmap(image)
 
 class RayTracerUI(wx.Frame):
     def __init__(self, *args, **kw):
@@ -12,51 +63,39 @@ class RayTracerUI(wx.Frame):
         self.InitUI()
 
     def InitUI(self):
-        self.image_width = 1280
-        self.image_height = 720
-        self.aspect_ratio = self.image_width / self.image_height
+        self.image_width = 640
+        self.image_height = 360
+        windowSize = (self.image_width + 25, self.image_height + 100)
 
-        pnl = wx.Panel(self)
-        # font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD)
-        # heading = wx.StaticText(pnl, label='Stats', pos=(105, 10))
-        # heading.SetFont(font)
+        self.SetSize(windowSize)
+        self.SetTitle('RayTracer')
+        self.SetAutoLayout(1)
 
-        self.renderBtn = wx.Button(pnl, label='Render', pos=(self.image_width - 85, 10))
-        self.renderBtn.Bind(wx.EVT_BUTTON, self.OnStartRender)
-        # refreshBtn = wx.Button(pnl, label='Manual Refresh', pos=(self.image_width / 2, 10))
-        # refreshBtn.Bind(wx.EVT_BUTTON, self.OnRefreshRender)
+        self.controls = ControlsPanel(self)
+        self.controls.renderBtn.Bind(wx.EVT_BUTTON, self.OnStartRender)
+        self.controls.saveBtn.Bind(wx.EVT_BUTTON, self.OnSave)
 
+        self.render_view = RenderPanel(self, self.ray_tracer_renderer.frame)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.controls, 0, wx.ALL | wx.EXPAND, 5)
+        sizer.Add(self.render_view, 1, wx.ALL | wx.ALIGN_CENTER | wx.SHAPED, 0)
+        self.SetSizer(sizer)
+
+        # refresh render result
         self.timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.OnRefreshTimer, self.timer)
 
+        # handle window close event
         self.Bind(wx.EVT_CLOSE, self.OnClose)
 
+        # handle window resize event
         self.resized = False # the dirty flag
         self.Bind(wx.EVT_SIZE, self.OnSize)
         self.Bind(wx.EVT_IDLE, self.OnIdle)
 
-        self.bitmap = self.MakeBitmap(self.ray_tracer_renderer.frame, self.image_width, self.image_height)
-        self.img = wx.StaticBitmap(pnl, 0, self.bitmap, pos=(5, 42))
-
-        windowSize = (self.image_width + 25, self.image_height + 100)
-        self.SetSize(windowSize)
-        self.SetTitle('RayTracer')
         self.Centre()
         self.Show(True)
-
-        self.img.SetBitmap(self.bitmap)
-
-    def RecalculateUIElements(self):
-        if not self.IsShown():
-            return
-
-        pos = (self.ClientSize.width - 95, 10)
-        self.renderBtn.SetPosition(pos)
-
-        offset_x = (self.ClientSize.width - self.image_width) / 2 - 1
-        offset_y = (self.ClientSize.height - self.image_height) / 2 + 21
-        imagePos = (offset_x, offset_y)
-        self.img.SetPosition(imagePos)
 
     def OnClose(self, event):
         self.ray_tracer_renderer.StopRendering()
@@ -72,26 +111,13 @@ class RayTracerUI(wx.Frame):
             new_width, new_height = self.GetSize()
             logging.info(f"w: {new_width}, h: {new_height}")
 
-            new_width -= 25
-            new_height -= 100
-
-            # self.aspect_ratio = w / h
-            new_ratio = new_width / new_height
-            if new_ratio < self.aspect_ratio:
-                self.image_width = new_width
-                self.image_height = self.image_width / self.aspect_ratio
-            else:
-                self.image_height = new_height
-                self.image_width = self.image_height * self.aspect_ratio
-
-            self.RecalculateUIElements()
             self.OnRefreshRender(event)
             self.Refresh()
             self.resized = False # reset the flag
 
     def InitRayTracer(self):
         # 80, 40, 20, 10, 8, 4, 2, 1
-        scale_down_factor = 2
+        scale_down_factor = 4
         self.ray_tracer_width = int(1280 / scale_down_factor)
         self.ray_tracer_height = int(720 / scale_down_factor)
         self.scene = Scene()
@@ -104,28 +130,38 @@ class RayTracerUI(wx.Frame):
             event.Skip()
             return
 
-        self.timer.Start(100)
+        self.started_render_time = time.perf_counter()
+        self.timer.Start(1000)
         self.ray_tracer_renderer.Render(self.scene)
 
     def OnRefreshTimer(self, event):
         # logging.debug('OnRefreshTimer')
+        self.controls.UpdateStats([
+            f'render time - {time.perf_counter() - self.started_render_time:0.4f}',
+            f'resolution - {self.ray_tracer_width}x{self.ray_tracer_height}'
+        ])
         if not self.ray_tracer_renderer.IsRendering():
             self.timer.Stop()
 
         self.OnRefreshRender(event)
 
     def OnRefreshRender(self, _event):
-        self.bitmap = self.MakeBitmap(self.ray_tracer_renderer.frame, self.image_width, self.image_height)
-        self.img.SetBitmap(self.bitmap)
+        self.render_view.UpdateRender(self.ray_tracer_renderer.frame)
 
-    def MakeBitmap(self, frame, target_width, target_height):
-        # Make a bitmap using an array of RGBA bytes
-        bitmap = wx.Bitmap.FromBufferRGBA(frame.width, frame.height, frame.bytes)
-        image = bitmap.ConvertToImage().Mirror(False)
-        # image = image.Scale(target_width, target_height, wx.IMAGE_QUALITY_HIGH)
-        image = image.Scale(target_width, target_height, wx.IMAGE_QUALITY_NEAREST)
-        return wx.Bitmap(image)
+    def OnSave(self, event):
+        today = datetime.today()
+        default_file_name = f'rt_{int(time.mktime(today.timetuple()))}'
+        # wildcard = "JPEG files (*.jpg)|*.jpg|BMP and GIF files (*.bmp;*.gif)|*.bmp;*.gif|PNG files (*.png)|*.png"
+        wildcard = "PNG files (*.png)|*.png"
 
+        dialog = wx.FileDialog(self,
+            "Save as...",
+            defaultFile = default_file_name,
+            wildcard = wildcard,
+            style = wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
+        if dialog.ShowModal() == wx.ID_OK: # Save button was pressed
+            self.render_view.bitmap.SaveFile(dialog.GetPath(), wx.BITMAP_TYPE_PNG)
+            dialog.Destroy()
 
 def main():
     rtApp = wx.App()
